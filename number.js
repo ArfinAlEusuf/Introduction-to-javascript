2f76fe25583d18789e1c6d3c51657af4e450e8e5
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-23 00:50:08 +0600

    complite 17.8

---
+++ number.js
@@ -0,0 +1,15 @@
+var orrangePrice = 20;
+var choolate = 0.5;
+// var applePrice = parseInt ('20');
+var applePrice = parseFloat ('20.25');
+
+
+
+// console.log(typeof orrangePrice)
+// console.log(typeof applePrice)
+// console.log(orrangePrice + applePrice);
+
+var first = 0.1;
+var second = 0.5;
+var total= first + second;
+// console.log(total.toFixed(1))
