1105c882a94a4824bf91fb557bac1548d17aa375
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-23 00:31:16 +0600

    complite 17.6

---
+++ naming.js
@@ -0,0 +1,9 @@
+// 1. No keyword as variable
+// 2. No space or gap
+// 3.no quote ""
+// 4.can not start with a number but you can use it after first letter
+// 5. name is case sensative
+// 6. how to write a long variable
+//    1.snake case (my_name) 
+//    2.camel case( myName)
+//    3.Pascal case (MyName)
