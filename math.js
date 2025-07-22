f1040515b40bb79d21e8ed30d60cde4997e34856
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-23 00:00:37 +0600

    js learning

---
+++ math.js
@@ -0,0 +1,16 @@
+/*
+math needed for programming
+
+1. number: +ve, -ve [50, -9]
+2.integer:2, 88 [odd, even]: 2,4,978
+3.float: 7,
+4. Operator
+5.More Operation
+6.percentage
+7.sum
+8.avg
+9.max/min
+10.series
+11.Area: Triangle
+12.
+ */ 
