ec4360ce806def3af92f8f1274e293c8aa92c98b
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-23 01:24:39 +0600

    shortHand

---
+++ shorthand.js
@@ -0,0 +1,15 @@
+var price = 35 ;
+// price =price +10;
+price+=10;   //price =price +10;
+console.log(price)
+
+
+var fridge = 35000;
+// fridge = fridge -100;
+fridge -= 1000;
+
+// multiply
+var orrangePrice = 30;
+orrangePrice= orrangePrice * 3;
+orrangePrice *= 3;
+console.log(orrangePrice);
