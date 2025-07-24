d1eb2d6aa8e5dc8e98f6e05b6154b8f810dc1343
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-24 21:57:03 +0600

    else if

--- stepped.js
+++ stepped.js
@@ -2,12 +2,18 @@
  * MULTI_LEVEL CONDITON
  */
 
-const price = 5000;
+const price = 4000;
 
 if(price >= 5000){
     const discount= price * 10 / 100;
     const payAmount= price - discount;
     console.log(payAmount);
+}
+else if(price > 2500){
+    // 5% discount
+    const discount = price * 5/100;
+    const payAmount= price - discount
+    console.log(payAmount)
 }
 else{
     console.log(price);
