3fa2b0e7663746f8c901988ab2b1a98fe3a44d0c
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-25 22:23:10 +0600

    logical not operator

--- shortcut.js
+++ shortcut.js
@@ -1,7 +1,17 @@
-const isLeader= true;
-if(isLeader === true){
-    console.log('leader asche chair charo');
+const isLeader = true;
+// if(isLeader === true){
+//     console.log('leader asche chair charo');
+// }
+// else{
+//     console.log('vitore dhukte dibi na');
+// }
+const isPassed = true;
+// if (isPassed === false) {
+//     console.log()
+// }
+if(!isPassed){
+    console.log('toke biye diye dibo');
 }
-else{
-    console.log('vitore dhukte dibi na');
+else {
+    console.log('tumi amder noyon er moni')
 }
