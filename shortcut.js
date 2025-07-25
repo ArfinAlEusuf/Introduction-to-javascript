1a85a5fa28d0968007d1391061f25bcaf827cb22
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-25 21:06:42 +0600

    shortcut

---
+++ shortcut.js
@@ -0,0 +1,7 @@
+const isLeader= true;
+if(isLeader === true){
+    console.log('leader asche chair charo');
+}
+else{
+    console.log('vitore dhukte dibi na');
+}
---
+++ ternary.js
@@ -0,0 +1,21 @@
+/**
+ * TERNARY--- three parts
+ * 
+ *    ?   :
+ * 
+ * condition ? do something when true : so some thing when wrong
+ */
+
+const age =20;
+// normal if else
+// if(age>= 18){
+//     console.log('you can vote')
+// }
+// else{
+//     console.log('Ghumai thako')
+// }
+
+// simple ternarry
+// age >= 18 ? console.log('Vote dio') : console.log('Ghumai thako')
+
+const price
